0b12ee0d0c828fb9408d74f9474bbdc95e50c4a7
Bjoern Vetter <vetterbjoern@yahoo.de>
2026-03-11 13:50:02 +0100

    logo and links

--- footer.php
+++ footer.php
@@ -22,19 +22,19 @@
       <ul class="social-media">
 
 <li>
-<a href="#">
+<a href="https://www.facebook.com/profile.php?id=100064602075734" target="_blank">
 <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg" alt="Facebook">
 </a>
 </li>
 
 <li>
-<a href="#">
+<a href="https://www.instagram.com/fineschliffdesign?fbclid=IwY2xjawQeP0BleHRuA2FlbQIxMABicmlkETBkZnUzSU9XQzZHalNRM0kzc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHjgkvK8AC_cB3e0LfQhX3yebeHFI3d6Rsx1jUXj3Wobn6opvC0ws0cKw36EK_aem_srs7wDQBWN4aCv5uHc4RjQ" target="_blank"> 
 <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/instagram.svg" alt="Instagram">
 </a>
 </li>
 
 <li>
-<a href="#">
+<a href="http://www.youtube.com/@Fineschliffdesign" target="_blank">
 <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/youtube.svg" alt="YouTube">
 </a>
 </li>
